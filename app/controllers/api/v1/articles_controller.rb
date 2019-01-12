2f041344708727c4a53c794e37ffc7467e2ebd3a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-12 17:39:39 -0500

    display all articles on database

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,26 +1,37 @@
 class Api::V1::ArticlesController < ApplicationController
+    before_action :set_article, only: [:show,:update,:destroy]
+
     def index
-      render json: Article.all
+      articles = Article.all
+      render json: articles, status: 200
     end
 
     def create
       article = Article.create(article_params)
-      render json: article
+      render json: article, status: 201
     end
 
-    def destroy
-      Article.destroy(params[:id])
+    def show
+      render json: @article, status: 200
     end
 
     def update
-      article = Article.find(params[:id])
-      article.update_attributes(article_params)
-      render json: article
+      @article.update(article_params)
+    end
+
+    def destroy
+      articleId = @article.id
+      @article.destroy
+      render json: {message:"Zap! Article deleted", articleId:articleId}
     end
 
     private
 
     def article_params
-      params.require(:article).permit(:id, :title, :text)
+      params.permit(:id, :title, :text)
+    end
+
+    def set_article 
+      @article = Article.find(params[:id])
     end
   end
