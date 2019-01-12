2f2247366fa0e6e838df5923e3eca4ec7517d73c
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-12 16:21:21 -0500

    rails backend connect to react frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Article.destroy_all
+
+10.times do
+  a = Article.create(
+    title: Faker::StarWars.character,
+    text: Faker::StarWars.quote
+  )
+
+  Article.create(title: a.title, text: a.text)
+
+end
