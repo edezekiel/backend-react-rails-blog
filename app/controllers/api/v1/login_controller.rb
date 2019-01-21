89cd7545c16995038e0df00db980323c5de5681e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-21 15:03:06 -0500

    still working on Auth

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,8 +1,8 @@
-class LoginController < ApplicationController
+class Api::V1::LoginController < ApplicationController
   def login
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      token = JWT.encode({ user_id: @user.id }, ENV['SECRET'], 'HS256')
+      token = JWT.encode({ user_id: @user.id }, 'mys3cr3t', 'HS256')
       render json: { token: token, name: @user.name }, status: :ok
     else
       render json: { errors: "Invalid credentials" },
